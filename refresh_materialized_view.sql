6a2d3bc995f2aa5cf2b8aec13e0bb10a68661f53
Vijay Mariadassou <vijay@karma3labs.com>
2023-10-12 18:28:13 -0700

    disabled refreshing views related to content feed in staging

--- refresh_materialized_view.sql
+++ refresh_materialized_view.sql
@@ -1,10 +1,10 @@
 refresh materialized view concurrently k3l_profiles;
-refresh materialized view k3l_posts;
+-- refresh materialized view k3l_posts;
 refresh materialized view concurrently k3l_follows;
 refresh materialized view concurrently k3l_mirrors;
 refresh materialized view concurrently k3l_comments;
 refresh materialized view concurrently k3l_collect_nft;
 refresh materialized view concurrently k3l_follow_counts;
-refresh materialized view concurrently k3l_feed;
-refresh materialized view concurrently k3l_following_feed;
+-- refresh materialized view concurrently k3l_feed;
+-- refresh materialized view concurrently k3l_following_feed;
 refresh materialized view concurrently k3l_rank;
