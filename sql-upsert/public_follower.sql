7edf5529230e8028420a2921bc3dbad2ec732017
Vijay Mariadassou <vijay@karma3labs.com>
2023-10-13 16:16:24 -0700

    fixed long running upsert sql locking up rds

--- sql-upsert/public_follower.sql
+++ sql-upsert/public_follower.sql
@@ -1,11 +1,14 @@
 -- For follower table
-INSERT INTO public.follower
-SELECT * FROM public.follower_tmp
-ON CONFLICT (address, follow_profile_id)
-DO UPDATE SET
-    block_timestamp = EXCLUDED.block_timestamp,
-    created_block_hash = EXCLUDED.created_block_hash,
-    is_finalised_on_chain = EXCLUDED.is_finalised_on_chain,
-    optimistic_id = EXCLUDED.optimistic_id,
-    uuid = EXCLUDED.uuid,
-    source_timestamp = EXCLUDED.source_timestamp;
+-- INSERT INTO public.follower
+-- SELECT * FROM public.follower_tmp
+-- ON CONFLICT (address, follow_profile_id)
+-- DO UPDATE SET
+--     block_timestamp = EXCLUDED.block_timestamp,
+--     created_block_hash = EXCLUDED.created_block_hash,
+--     is_finalised_on_chain = EXCLUDED.is_finalised_on_chain,
+--     optimistic_id = EXCLUDED.optimistic_id,
+--     uuid = EXCLUDED.uuid,
+--     source_timestamp = EXCLUDED.source_timestamp;
+TRUNCATE TABLE public.follower; 
+INSERT INTO public.follower 
+    SELECT * FROM public.follower_tmp ON CONFLICT DO NOTHING;
