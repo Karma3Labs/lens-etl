2f2db0714faa737c9ac240368a2f93ed4adce1d9
Jacksteroo <1073117+jacksteroo@users.noreply.github.com>
2023-10-27 21:23:33 -0700

    Convert profile and follower to Lens v2

--- sql-import-update/public_profile.sql
+++ sql-import-update/public_profile.sql
@@ -7,36 +7,38 @@ EXPORT DATA
     field_delimiter = ',')
 AS (
   SELECT 
-    profile_id, 
-    name, 
-    handle,
-    profile_with_weights, 
-    profile_picture_s3_url, 
-    cover_picture_s3_url, 
-    is_profile_picture_onchain,
-    owned_by,
-    is_burnt,
-    nft_contract,
-    nft_token_id,
-    nft_owner,
-    nft_chain_id,
-    is_default,
-    metadata_url,
-    metadata_s3_url,
-    is_metadata_processed,
-    has_error,
-    timeout_request,
-    metadata_error_reason,
-    app_id,
-    metadata_block_timestamp,
-    metadata_created_block_hash,
-    metadata_tx_hash,
-    block_timestamp,
-    created_block_hash,
-    datastream_metadata.uuid,
-    datastream_metadata.source_timestamp
+    pr.profile_id,
+    pm.name,
+    nh.local_name AS handle,
+    pm.profile_with_weights,
+    pm.profile_picture_snapshot_location_url AS profile_picture_s3_url,
+    pm.cover_picture_snapshot_location_url AS cover_picture_s3_url,
+    false AS is_profile_picture_onchain,
+    pr.owned_by,
+    pr.is_burnt,
+    NULL AS nft_contract,
+    NULL AS nft_token_id,
+    NULL AS nft_owner,
+    NULL AS nft_chain_id,
+    false AS is_default,
+    pm.metadata_uri AS metadata_url,
+    pm.metadata_snapshot_location_url AS metadata_s3_url,
+    NULL AS is_metadata_processed,
+    NULL AS has_error,
+    NULL AS timeout_request,
+    NULL AS metadata_error_reason,
+    pm.app AS app_id,
+    pm.block_timestamp AS metadata_block_timestamp,
+    pm.block_hash AS metadata_created_block_hash,
+    pm.tx_hash AS metadata_tx_hash,
+    pr.block_timestamp AS block_timestamp,
+    pr.block_hash AS created_block_hash,
+    pr.datastream_metadata.uuid, 
+    pr.datastream_metadata.source_timestamp 
   FROM
-    public_profile
+    profile_record AS pr
+    JOIN profile_metadata AS pm ON pm.profile_id = pr.profile_id
+    JOIN namespace_handle AS nh ON nh.owned_by = pr.owned_by
   WHERE
     block_timestamp > 'BLOCK_TIMESTAMP'
   ORDER BY
