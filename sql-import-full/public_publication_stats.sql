d6c2c2aa9f4a18bb9632ec91d7b2274bd5ed814a
Vijay Mariadassou <vijay@karma3labs.com>
2023-10-30 18:34:37 -0700

    migration to v2

--- sql-import-full/public_publication_stats.sql
+++ sql-import-full/public_publication_stats.sql
@@ -6,17 +6,28 @@ EXPORT DATA
     header = true,
     field_delimiter = ',')
 AS (
-  SELECT 
-    publication_id,
-    total_amount_of_collects,
-    total_amount_of_mirrors,
-    total_amount_of_comments,
-    total_upvotes,
-    total_downvotes,
-    datastream_metadata.uuid, 
-    datastream_metadata.source_timestamp 
+  WITH reaction AS (
+    select
+      publication_id,
+      SUM(CASE WHEN reaction_type='UPVOTE' THEN total ELSE 0 END) as total_upvotes,
+      SUM(CASE WHEN reaction_type='DOWNVOTE' THEN total ELSE 0 END) as total_downvotes
+    from 
+      global_stats_publication_reaction
+    group by publication_id
+  )
+  SELECT
+    stats.publication_id,
+    stats.total_amount_of_collects,
+    stats.total_amount_of_mirrors,
+    stats.total_amount_of_comments,
+    ifnull(reaction.total_upvotes, 0) as total_upvotes,
+    ifnull(reaction.total_downvotes, 0) as total_downvotes,
+    stats.datastream_metadata.uuid, 
+    stats.datastream_metadata.source_timestamp 
   FROM
-    public_publication_stats
+  global_stats_publication as stats
+  LEFT JOIN reaction
+            ON (stats.publication_id=reaction.publication_id)
   ORDER BY
     publication_id
 );
