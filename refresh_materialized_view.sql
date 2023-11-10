53e30961d6a23ead5cfa1d9211cc36a63d63d463
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-09 21:36:40 -0800

    changed follow_counts to refresh concurrently

--- refresh_materialized_view.sql
+++ refresh_materialized_view.sql
@@ -4,7 +4,7 @@ refresh materialized view concurrently k3l_follows;
 refresh materialized view k3l_mirrors;
 refresh materialized view k3l_comments;
 refresh materialized view k3l_collect_nft;
-refresh materialized view k3l_follow_counts;
+refresh materialized view concurrently k3l_follow_counts;
 refresh materialized view k3l_upvotes;
 refresh materialized view k3l_interactions;
 refresh materialized view concurrently k3l_feed;
