555216fec0f4a571cf13affc1d3dcac76d5373b3
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-09 21:48:57 -0800

    added todos to fix refresh order to match compute order

--- refresh_materialized_view.sql
+++ refresh_materialized_view.sql
@@ -7,6 +7,10 @@ refresh materialized view k3l_collect_nft;
 refresh materialized view concurrently k3l_follow_counts;
 refresh materialized view k3l_upvotes;
 refresh materialized view k3l_interactions;
+
+-- TODO k3l_feed and k3l_following_feed should be refreshed after compute feed runs
 refresh materialized view concurrently k3l_feed;
 refresh materialized view concurrently k3l_following_feed;
+
+-- TODO k3l_rank should be refreshed after compute runs and before compute feed
 refresh materialized view concurrently k3l_rank;
